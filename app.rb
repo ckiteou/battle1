7a207ed2ac9c21ea13b0572cc519b95ee7b9927c
Costas Kiteou <ckiteou22@gmail.com>
2017-07-20 18:49:09 +0100

    Made controller skinnier by accessing Player instances through the Game class

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
-require './lib/player.rb'
+require './lib/game'
+require './lib/player'
 
 class Battle < Sinatra::Base
 
@@ -8,22 +9,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_one])
-    $player_2 = Player.new(params[:player_two])
-    p $player_1
+    player_1 = Player.new(params[:player_one])
+    player_2 = Player.new(params[:player_two])
+    $game = Game.new(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $player_1
-    @player_2 = $player_2
+    @game = $game
     erb(:play)
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    Game.new.attack(@player_2)
+    @game = $game
+    @game.attack(@game.player_2)
     erb :attack
   end
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,17 @@
 class Game
+
+  def initialize(players_1, player_2)
+    @players = [players_1, player_2]
+  end
+
+  def player_1
+    @players.first
+  end
+
+  def player_2
+    @players.last
+  end
+
   def attack(player)
     player.receive_damage
   end
